9d660eeb38aaa1430266f8c562d64e8d26d25399
Shivam Dhuria <sdhuria@ask-fast.com>
2021-04-09 23:34:11 +0530

    build successfully for android

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,9 +5,14 @@ plugins {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("com.google.android.material:material:1.2.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
 
 android {
--- androidApp/src/main/java/com/elixer/paws/android/MainActivity.kt
+++ androidApp/src/main/java/com/elixer/paws/android/MainActivity.kt
@@ -2,19 +2,67 @@ package com.elixer.paws.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.elixer.paws.Greeting
-import android.widget.TextView
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import com.elixer.paws.shared.SpaceXSDK
+import com.elixer.paws.shared.cache.DatabaseDriverFactory
+import kotlinx.coroutines.cancel
 
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
+    private val mainScope = MainScope()
+
+    private lateinit var launchesRecyclerView: RecyclerView
+    private lateinit var progressBarView: FrameLayout
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    private val sdk = SpaceXSDK(DatabaseDriverFactory(this))
+
+    private val launchesRvAdapter = LaunchesRvAdapter(listOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        title = "SpaceX Launches"
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        launchesRecyclerView = findViewById(R.id.launchesListRv)
+        progressBarView = findViewById(R.id.progressBar)
+        swipeRefreshLayout = findViewById(R.id.swipeContainer)
+
+        launchesRecyclerView.adapter = launchesRvAdapter
+        launchesRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            displayLaunches(true)
+        }
+
+        displayLaunches(false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainScope.cancel()
+    }
+
+    private fun displayLaunches(needReload: Boolean) {
+        progressBarView.visibility = View.VISIBLE
+        mainScope.launch {
+            kotlin.runCatching {
+                sdk.getLaunches(needReload)
+            }.onSuccess {
+                launchesRvAdapter.launches = it
+                launchesRvAdapter.notifyDataSetChanged()
+            }.onFailure {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+            progressBarView.visibility = View.INVISIBLE
+        }
     }
 }
